d5bbaacb8c97129cbbb257fe95099fc3b5874691
Jun C <kjstudios858@gmail.com>
2024-09-29 10:27:38 -0700

    Refactor roc.py

--- jcds/charts/roc.py
+++ jcds/charts/roc.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn import metrics 
 
@@ -8,4 +9,5 @@ def plot_roc(y_test, y_pred, model_name):
     fpr, tpr, _ = metrics.roc_curve(y_test, y_pred)
     auc = round(metrics.roc_auc_score(y_test, y_pred), 4)
     label = f"{model_name}, AUC={auc}"
-    return(fpr, tpr, label)
+    # return(fpr, tpr, label)
+    plt.plot(fpr, tpr, label = label)
