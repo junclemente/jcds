632696ff2ec5d0cf7c62eb461bc11ab9bdd18145
Jun Clemente <junclemente@users.noreply.github.com>
2024-09-29 09:00:08 -0700

    Changed folder names
    Add new dataset

--- jcds/charts/roc.py
+++ jcds/charts/roc.py
@@ -1,2 +1,11 @@
 import pandas as pd 
 import numpy as np
+
+from sklearn import metrics 
+
+
+def plot_roc(y_test, y_pred, model_name):
+    fpr, tpr, _ = metrics.roc_curve(y_test, y_pred)
+    auc = round(metrics.roc_auc_score(y_test, y_pred), 4)
+    label = f"{model_name}, AUC={auc}"
+    return(fpr, tpr, label)
