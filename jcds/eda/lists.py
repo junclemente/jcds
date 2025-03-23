86b5fcab63cd5f21deb62af9a803c30bbba22c8f
Jun Clemente <junclemente@users.noreply.github.com>
2025-03-23 12:48:26 -0700

    Add docstrings

--- jcds/eda/lists.py
+++ jcds/eda/lists.py
@@ -1,10 +1,36 @@
 import pandas as pd
 import numpy as np
 
+
 def get_cat_list(dataframe):
-    list = dataframe.select_dtypes(include=['category', 'object']).columns.tolist()
-    return list
+    """
+    Returns a list of categorical column names or the subset of the DataFrame
+    containing only categorical columns.
+
+    Args:
+        dataframe (pd.DataFrame): The DataFrame to analyze.
+        return_names (bool): If True, returns a list of column names.
+                             If False, returns a DataFrame slice.
+
+    Returns:
+        list or pd.DataFrame: Categorical column names or DataFrame with categorical columns.
+    """
+    cat_list = dataframe.select_dtypes(include=["category", "object"]).columns.tolist()
+    return cat_list
+
 
 def get_cont_list(dataframe):
-    list = dataframe.select_dtypes(exclude=['category', 'object']).columns.tolist()
-    return list
+    """
+    Returns a list of continuous (non-categorical) column names or the subset of the DataFrame
+    containing only continuous columns.
+
+    Args:
+        dataframe (pd.DataFrame): The DataFrame to analyze.
+        return_names (bool): If True, returns a list of column names.
+                             If False, returns a DataFrame slice.
+
+    Returns:
+        list or pd.DataFrame: Continuous column names or DataFrame with continuous columns.
+    """
+    cont_list = dataframe.select_dtypes(exclude=["category", "object"]).columns.tolist()
+    return cont_list
