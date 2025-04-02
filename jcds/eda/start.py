f1f2cc7777f9168b2dac1d9cb627f407f80837de
Jun Clemente <junclemente@users.noreply.github.com>
2025-04-01 17:00:52 -0700

    Create version 0.2.1
    Update to include pip installation

--- jcds/eda/start.py
+++ jcds/eda/start.py
@@ -1,26 +1,20 @@
-from IPython.display import Markdown, display
+# from IPython.display import Markdown, display
 
-def eda_guide_markdown():
-    md_text = """
-# 🧭 EDA Guide Overview
+# def eda_guide_markdown():
+#     md_text = """
+# # 🧭 EDA Guide Overview
 
-Welcome to your Exploratory Data Analysis journey!  
-Follow this structured checklist to deeply understand your dataset and prepare it for modeling.
+# Welcome to your Exploratory Data Analysis journey!
+# Follow this structured checklist to deeply understand your dataset and prepare it for modeling.
 
----
+# ---
 
-## 📦 Step 0: Import the Data  
-Load your dataset into a Pandas DataFrame. For example:  
-```python
-import pandas as pd
-df = pd.read_csv("your_dataset.csv")" \
-""" 
-
-
-
-
-
-
+# ## 📦 Step 0: Import the Data
+# Load your dataset into a Pandas DataFrame. For example:
+# ```python
+# import pandas as pd
+# df = pd.read_csv("your_dataset.csv")" \
+# """
 
 
 # ===========================================
@@ -31,20 +25,20 @@ df = pd.read_csv("your_dataset.csv")" \
 # duplicates
 # correct datatypes
 # outliers
-# fix encoding issues 
+# fix encoding issues
 
 # Univariate analysis:
-# Numerical: 
+# Numerical:
 # histograms
 # KDE plots
-# boxplots 
+# boxplots
 
 # Categorical:
 # value counts
 # bar plots
 
 # Bivariate/Multivariate analysis:
-# relationship between variables 
+# relationship between variables
 # target vs feature (classification vs regression)
 # Correlation matrix, heatmaps, pairplots
 # Crosstabs for categorical comparison
