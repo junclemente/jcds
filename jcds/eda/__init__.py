f19d30aaeea8bea88438033da599d555b4c04142
Jun Clemente <kjstudios858@gmail.com>
2025-04-08 11:04:51 -0700

    Update help

--- jcds/eda/__init__.py
+++ jcds/eda/__init__.py
@@ -1,4 +1,4 @@
-import inspect
+import inspect as pyinspect
 
 # Import your functions from internal files
 from .datetime import create_dt_col, create_dt_cols
@@ -84,6 +84,6 @@ def help(func_name=None):
         func = functions.get(func_name)
         if func:
             print(f"\nHelp for '{func_name}':\n")
-            print(inspect.getdoc(func) or "(No docstring provided)")
+            print(pyinspect.getdoc(func) or "(No docstring provided)")  # 👈 change here
         else:
             print(f"Function '{func_name}' not found.")
