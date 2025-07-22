4ed4497ad47fafc4f1f965e8d246a32eaabfbf78
Jun Clemente <junclemente@users.noreply.github.com>
2025-07-21 19:07:00 -0700

    Update test_outliers.py

--- tests/test_outliers.py
+++ tests/test_outliers.py
@@ -1,5 +1,9 @@
+import pandas as pd
+from jcds.eda import outliers
+
+
 def test_plot_outlier_boxplots(sample_df):
     try:
-        plot_outlier_boxplots(sample_df)
+        outliers.plot_outlier_boxplots(sample_df)
     except Exception as e:
         pytest.fail(f"plot_outlier_boxplots raised an exception: {e}")
